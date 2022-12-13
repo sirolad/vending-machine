3da5c45003de864ad231c98491056b91615623e1
Surajudeen Akande <surajudeen.akande@jsmd-group.com>
2022-12-13 15:39:38 +0100

    handle 404 customly as ORM doesn't

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { EntityNotFoundExceptionFilter } from './infrastructure/filters/entity-not-found-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
   app.setGlobalPrefix('api');
 
   const options = new DocumentBuilder()
