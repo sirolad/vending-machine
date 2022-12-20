1983c7f7ac2e4e476b70b19400c6e319a8c0a2d6
Surajudeen Akande <surajudeen.akande@jsmd-group.com>
2022-12-20 12:22:20 +0100

    use one coin source

--- src/infrastructure/validators/is-valid-coin.validator.ts
+++ src/infrastructure/validators/is-valid-coin.validator.ts
@@ -2,12 +2,14 @@ import {
   ValidatorConstraint,
   ValidatorConstraintInterface,
 } from 'class-validator';
+import { CoinsBreaker } from '../../domain';
 
 @ValidatorConstraint({ name: 'validCoin' })
 export class IsValidCoin implements ValidatorConstraintInterface {
+  constructor(private readonly coinsBreaker: CoinsBreaker) {}
   validate(value: number): boolean {
     if (value) {
-      return [5, 10, 20, 50, 100].includes(value);
+      return this.coinsBreaker.coins.includes(value);
     }
     return false;
   }
